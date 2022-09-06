af4d3fc956d30209914804b29cd54739e75d7093
Shree <lyfizhere@gmail.com>
2022-09-06 16:08:11 +0530

    Started codewars challenges with 8kyu

---
+++ 8kyu.js
@@ -0,0 +1,9 @@
+//01. Convert a Number to a String!!
+
+//My Solution - 
+function numberToString(num) {
+    let str = num.toString();
+    return str;
+}
+numberToString(123);
+console.log(numberToString(123));
